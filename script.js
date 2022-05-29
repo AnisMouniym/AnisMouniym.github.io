092d99302647aeb225819891400155d32c3b84ab
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2022-05-29 15:48:19 +0200

    add typed text

---
+++ script.js
@@ -0,0 +1,9 @@
+// TYPED
+var typed3 = new Typed('.typed', {
+    strings: ['Coucou','Salut','Bonjour','Bonsoir','Bonne nuit','Bonne journée'],
+    typeSpeed: 100,
+    backSpeed: 0,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
+    
