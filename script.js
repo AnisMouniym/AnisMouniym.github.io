78835106e499a503167dd4a63fefec516ee9b5f4
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2022-05-29 16:15:14 +0200

    picture, favicon and text change

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // TYPED
 var typed3 = new Typed('.typed', {
-    strings: ['Coucou','Salut','Bonjour','Bonsoir','Bonne nuit','Bonne journée'],
-    typeSpeed: 100,
-    backSpeed: 0,
+    strings: ['un animateur','un étudiant','un ingénieur','un développeur','Mouniym Anis !'],
+    typeSpeed: 30,
+    backSpeed: 60,
     smartBackspace: true, // this is a default
-    loop: true
+    loop: false
   });
     
