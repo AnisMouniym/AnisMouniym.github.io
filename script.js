71d8211f5fd9013b8f972cbcab9cb19de75fd9e4
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2022-06-20 17:29:02 +0200

    h1,h2,h3 font change and typed text lenght change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // TYPED
 var typed3 = new Typed('.typed', {
-    strings: ['un animateur','un étudiant','un ingénieur','un développeur','Mouniym Anis !'],
+    strings: ["un étudiant ",'un développeur','Mouniym Anis !'],
     typeSpeed: 30,
     backSpeed: 60,
     smartBackspace: true, // this is a default
