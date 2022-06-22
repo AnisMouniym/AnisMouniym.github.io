1a65e6f2291b59362b5917e2557e3dd5adc08dcb
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2022-06-23 00:18:29 +0200

    add langages progress bar

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// NAVBAR
+let lastScrollTop = 0;
+let navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll',function() {
+  const scrollTop = window.pageXOffset ||
+  this.document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top='-50px';
+  } else {
+    navbar.style.top='0';
+  }
+  lastScrollTop = scrollTop;
+});
+
+
 // TYPED
 var typed3 = new Typed('.typed', {
     strings: ["Je suis un étudiant ",'Je suis un développeur','Je suis Mouniym Anis ! ',' Et voici ci-dessous mes projets:'],
