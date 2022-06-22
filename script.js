37813e86b9fd29c97ce05440f544798102702388
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2022-06-22 17:17:53 +0200

    boulder dash's map change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // TYPED
 var typed3 = new Typed('.typed', {
-    strings: ["Je suis un étudiant ",'Je suis un développeur','Je suis Mouniym Anis ! ',' Et voici ci dessous mes projets:'],
+    strings: ["Je suis un étudiant ",'Je suis un développeur','Je suis Mouniym Anis ! ',' Et voici ci-dessous mes projets:'],
     typeSpeed: 30,
     backSpeed: 60,
     smartBackspace: true, // this is a default
